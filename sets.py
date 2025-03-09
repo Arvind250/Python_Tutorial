95bde40e30da1cf6216a70612527174e507d10e6
Arvind250 <arvindchoudhary37903@gmail.com>
2025-03-09 10:32:58 +0530

    Delete unwanted files

--- sets.py
+++
@@ -1,5 +0,0 @@
-# my_set={1,2,2,3,3}
-# print(len(my_set),my_set)
-
-my_set= set()
-print(len(my_set),my_set)
--- tuples.py
+++
@@ -1,7 +0,0 @@
-my_tuple=(1,2,3,4,"hello")
-print(type(my_tuple))
-print(my_tuple)
-print(my_tuple[1])
-(a,b,c,d,e) = my_tuple
-print(type(a),a)
-print(type(e),e)
