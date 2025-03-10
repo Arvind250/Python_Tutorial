51bfc95a257ada6b54d14fd6f9ff805c020daec1
Arvind250 <arvindchoudhary37903@gmail.com>
2025-03-10 22:17:29 +0530

    Function files added

---
+++ Functions/01.py
@@ -0,0 +1,4 @@
+def my_func(a):
+    return a**2
+result = my_func(3)
+print(my_func(3))
---
+++ Functions/02.py
@@ -0,0 +1,6 @@
+# Create a function that takes two numbers as parameters and return their sum.
+
+def sum(a,b):
+    return a+b
+print(sum(2,6))
+print(20.45+1284.35)
